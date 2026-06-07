8ade1e0766963c6ee983830c4a994bd6d7b95d45
Kawê <kawe.apr@gmail.com>
2026-06-07 18:47:42 -0300

    UP

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -41,7 +41,7 @@ export default function SetupPage() {
             {/* Top Navigation */}
             <nav className="w-full flex justify-between items-center px-4 py-4 bg-white shadow-md">
                 <Link href="/" className="text-blue-600 font-semibold">← Voltar</Link>
-                <h1 className="text-lg font-bold">Começar</h1>
+                <h1 className="text-lg font-bold">Avaliação de Usabilidade do Sistema Roadmap {" { "}TI{" } "}</h1>
                 <div className="w-16" /> {/* Placeholder para equilibrar o espaço */}
             </nav>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdk41w4czpcyVP_G1wnk5PDfPtGS1T-vjBwJDCmbGb-x1Rhtw/viewform?embedded=true" width="640" height="700" >Carregando…</iframe>
